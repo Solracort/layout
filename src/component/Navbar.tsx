6f5c09213b53b08eab0b707f285e3b8d27d3ecdb
solracort <solracort@gmail.com>
2023-02-28 21:32:32 +0100

    beta english version

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
 				<img id="casita" src={Inicio} alt="Home"/><span id="inicio">Home</span>
 			</a>
 			</li>
-			<li key="1"><Link to={"./Bio/"+{lang}}>ENGLISH version</Link></li>
+			<li key="1"><Link to={"/bmañana io/"+{lang}}>ENGLISH version</Link></li>
 			<li key="2" className="nav-boton">
 				<a className="nav-link" rel="noreferrer" target="_blank" href="https://drive.google.com/file/d/1nRdh5nP7EC53lw4EtUeMUitk_2PwEY11/view?usp=sharing" role="button" >CV</a>
 			</li>
