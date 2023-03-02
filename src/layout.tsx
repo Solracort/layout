463903a56b6c1ccfb3485ed3ca705ee1503fff16
solracort <solracort@gmail.com>
2023-03-02 11:19:56 +0100

    English version and Flags fixed

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from './component/App';
 // import { Home } from "./views/home";
-import { Bio } from "./component/Bio";
+// import { Bio } from "./component/Bio";
 import { Navbar } from "./component/Navbar";
 import { Footer } from "./component/Footer";
 
@@ -17,7 +17,7 @@ const Layout = () => {
 			<BrowserRouter basename={basename}>
 					<Navbar />
 					<Routes>
-						<Route path="/bio/:lang" element={<App />} />
+						<Route path="/Bio/:lang" element={<App />} />
 						<Route path="/" element={<App />} />
 						{/* <Route path="*" element={<h1>Not found!</h1>} />	 */}
 					</Routes>
